abe48912784d4a6b61cd1d70a672c5009fc857e8
Jeff Zohrab <jzohrab@gmail.com>
2023-05-07 19:35:59 -0700

    Script working.

--- lutelemma/lemmatize.py
+++ lutelemma/lemmatize.py
@@ -1,3 +1,70 @@
-class Hello:
-    def hi(self):
-        print('hello')
+import os
+
+# https://www.geeksforgeeks.org/break-list-chunks-size-n-python/
+# Yield successive n-sized
+# chunks from arr.
+def divide_chunks(arr, n):
+    # looping till length arr
+    for i in range(0, len(arr), n):
+        yield arr[i:i + n]
+
+
+def get_child_parent_pairs(langcode, lines):
+    linegroups = list(divide_chunks(lines, 10))
+    groups = [ ''.join(g).strip() for g in linegroups ]
+
+    print("Opening library ...")
+    from spacy_stanza import load_pipeline
+    print("Done.")
+
+    currdir = os.path.dirname(os.path.abspath(__file__))
+    downloaddir = os.path.join(currdir, '..', 'models')
+    langdir=os.path.join(downloaddir, langcode)
+
+    if (not os.path.exists(downloaddir)):
+        os.mkdir(downloaddir)
+
+    if (not os.path.exists(langdir)):
+        print(f'Path {langdir} exists, model already downloaded.')
+
+    if (not os.path.exists(langdir)):
+        print(f"downloading {langcode} model to {langdir}")
+        import stanza
+        stanza.download(langcode, model_dir=downloaddir, processors="tokenize,mwt,pos,lemma")
+        print("Done.")
+
+    print("Loading pipeline ...")
+    nlp = load_pipeline(    
+        langcode,
+        dir=downloaddir,
+        processors="tokenize,mwt,pos,lemma",
+        download_method=None,
+        logging_level='FATAL'
+    )
+    print(f"Done.  Processing {len(groups)} batches.")
+
+    ret = []
+    n = 0
+    numgroups = len(groups)
+    for text in groups:
+        n += 1
+        print(f'  {n} of {numgroups}')
+        doc = nlp(text)
+        child_parent = [
+            [ token.text.strip(), token.lemma_.strip() ]
+            for token in doc if token.text.strip() != ''
+        ]
+        ret.extend([ p for p in child_parent if p[0] != p[1] ])
+
+    return ret
+
+
+def generate_import(langcode, lines, writer):
+    arr = get_child_parent_pairs(langcode, lines)
+    # print(arr)
+    by_parent_length = sorted(
+        arr,
+        key=lambda x: '_'.join([ '%03d' % len(x[1]), x[1], x[0] ])
+    )
+    for p in by_parent_length:
+        writer.write(f"{p[0]}\t{p[1]}\n")
--- main.py
+++ main.py
@@ -1,21 +1,25 @@
 import sys
 import os
-from lutelemma.lemmatize import Hello
+from lutelemma.lemmatize import generate_import
 
 ARGS=sys.argv
-if (len(ARGS) != 3):
-    print("Need lang code, infile")
+if (len(ARGS) != 4):
+    print("Need lang code, infile, outfile")
     sys.exit(1)
 
 langcode = ARGS[1]
 infile = ARGS[2]
+outfile = ARGS[3]
 
 if (not os.path.exists(infile)):
     print(f"Missing input file {infile}")
     sys.exit(1)
 
-print(langcode)
-print(infile)
+lines = []
+with open(infile, 'r') as reader:
+    lines = reader.readlines()
 
-h = Hello()
-h.hi()
+with open(outfile, 'w') as writer:
+    generate_import(langcode, lines, writer)
+
+print(f"\nFile generated: {outfile}")
