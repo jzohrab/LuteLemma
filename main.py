d6a851e1a39360e37eb9e26dd85fd0b99bac4350
Jeff Zohrab <jzohrab@gmail.com>
2023-05-07 17:37:30 -0700

    Get args.

--- main.py
+++ main.py
@@ -1,4 +1,21 @@
+import sys
+import os
 from lutelemma.lemmatize import Hello
+
+ARGS=sys.argv
+if (len(ARGS) != 3):
+    print("Need lang code, infile")
+    sys.exit(1)
+
+langcode = ARGS[1]
+infile = ARGS[2]
+
+if (not os.path.exists(infile)):
+    print(f"Missing input file {infile}")
+    sys.exit(1)
+
+print(langcode)
+print(infile)
 
 h = Hello()
 h.hi()
